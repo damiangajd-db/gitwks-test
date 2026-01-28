af737bab5dd15a7520b58b93a019f41a0d246740
damiangajd-db <damiangajd-db>
2026-01-28 00:25:53 +0000

    dasdsad

--- folder/print_me.py
+++ folder/print_me.py
@@ -1,3 +1,6 @@
 # simple printing function
 def print_me(some_message):
   print("print from print_me: " + some_message)
+
+def print_me4(some_message):
+  print("print from print_me4: " + some_message)
